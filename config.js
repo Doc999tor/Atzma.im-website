1a06bf5e88abfee9060abad6048ab97ab7159890
Victor <doc999tor@gmail.com>
2019-05-26 21:58:08 +0300

    translations

--- config.js
+++ config.js
@@ -2,12 +2,7 @@ var config = {
   locale: 'en',
   isRTL: false,
   modules: {
-    hero: {
-      // internal_link: { // I'm not sure if hero should have a link
-      //   name: 'hero',
-      //   url: 'hero',
-      // },
-    },
+    hero: {},
     why: {
       internal_link: {
         name: 'why',
@@ -176,73 +171,4 @@ var config = {
     media_clients: './dist/clients/',
     media_business_types: './dist/business_types/'
   },
-  translations: {
-    hero: {
-      internal_link_name: 'HERO',
-      main_logo: 'ATZMAIM',
-      main_logo_label: 'ATZMAIM Logo',
-      main_title: 'Manage Business Easily',
-      log_in: 'Log in',
-      sign_up: 'Sign Up',
-      description: 'We’ve created for you an application with calendar,clients appointments, automated text reminders andother useful tools for managing your business easy',
-      join_us: 'Join Now for Free',
-      calendar_icon: 'Calendar picture'
-    },
-    why: {
-      internal_link_name: 'WHY',
-      content: {
-        why_atmza: 'Why Atzmaim',
-        why_description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
-        business: 'Business Management',
-        appointments: 'Appointments Calendar',
-        notifications: 'SMS Notifications',
-        subscriptions: 'Client’s Subscriptions',
-        management: 'Client’s Management',
-        reminders: 'Tasks and Reminders'
-      }
-    },
-    showcases: {
-      internal_link_name: 'SHOWCASES',
-      main_title: 'App that makes business easy',
-      learn_more: 'Learn More',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua',
-      icon_alt_text: 'Iphone icon'
-    },
-    business_types: {
-      internal_link_name: 'BUSINESS TYPES',
-      main_title: 'Business Types',
-      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
-      content: {   // * 6 items
-        hair_salons: {
-          title: 'Hair Salons Title',
-          text: 'Hair Salons Text',
-          alt: 'Hair Salons Alt',
-        },
-        massage_centers: {
-          title: 'Massage Centers Title',
-          text: 'Massage Centers Text',
-          alt: 'Massage Centers Alt',
-        },
-        sports_fitness: {
-          title: 'Sports Fitness Title',
-          text: 'Sports Fitness Text',
-          alt: 'Sports Fitness Alt',
-        },
-      }
-    },
-    feedback: {
-      internal_link_name: 'FEEDBACK',
-      alt_pic: 'User avatar',
-      main_title: 'Feedback from our customers',
-      subtitle: `Here you can leave your feedback about our
-    product. Reviews are publishing instantly`,
-      leave_btn_label: 'Leave Feedback'
-    },
-    footer: {
-      copy_right: '{year} Atzmaim | All right reserved',
-      twitter: 'Twitter',
-      facebook: 'Facebook',
-      instagram: 'Instagram'
-    }
-  }
 }
---
+++ translations/all.js
@@ -0,0 +1,209 @@
+var all_translations = {
+	en: {
+    hero: {
+      internal_link_name: 'HERO',
+      main_logo: 'ATZMAIM',
+      main_logo_label: 'ATZMAIM Logo',
+      main_title: 'Manage Business Easily',
+      log_in: 'Log in',
+      sign_up: 'Sign Up',
+      description: 'We’ve created for you an application with calendar,clients appointments, automated text reminders andother useful tools for managing your business easy',
+      join_us: 'Join Now for Free',
+      calendar_icon: 'Calendar picture'
+    },
+    why: {
+      internal_link_name: 'WHY',
+      content: {
+        why_atmza: 'Why Atzmaim',
+        why_description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+        business: 'Business Management',
+        appointments: 'Appointments Calendar',
+        notifications: 'SMS Notifications',
+        subscriptions: 'Client’s Subscriptions',
+        management: 'Client’s Management',
+        reminders: 'Tasks and Reminders'
+      }
+    },
+    showcases: {
+      internal_link_name: 'SHOWCASES',
+      main_title: 'App that makes business easy',
+      learn_more: 'Learn More',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua',
+      icon_alt_text: 'Iphone icon'
+    },
+    business_types: {
+      internal_link_name: 'BUSINESS TYPES',
+      main_title: 'Business Types',
+      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+      content: {   // * 6 items
+        hair_salons: {
+          title: 'Hair Salons Title',
+          text: 'Hair Salons Text',
+          alt: 'Hair Salons Alt',
+        },
+        massage_centers: {
+          title: 'Massage Centers Title',
+          text: 'Massage Centers Text',
+          alt: 'Massage Centers Alt',
+        },
+        sports_fitness: {
+          title: 'Sports Fitness Title',
+          text: 'Sports Fitness Text',
+          alt: 'Sports Fitness Alt',
+        },
+      }
+    },
+    feedback: {
+      internal_link_name: 'FEEDBACK',
+      alt_pic: 'User avatar',
+      main_title: 'Feedback from our customers',
+      subtitle: `Here you can leave your feedback about our
+    product. Reviews are publishing instantly`,
+      leave_btn_label: 'Leave Feedback'
+    },
+    footer: {
+      copy_right: '{year} Atzmaim | All right reserved',
+      twitter: 'Twitter',
+      facebook: 'Facebook',
+      instagram: 'Instagram'
+    }
+  },
+  ru: {
+    hero: {
+      internal_link_name: 'HERO',
+      main_logo: 'ATZMAIM',
+      main_logo_label: 'ATZMAIM Logo',
+      main_title: 'Manage Business Easily',
+      log_in: 'Log in',
+      sign_up: 'Sign Up',
+      description: 'We’ve created for you an application with calendar,clients appointments, automated text reminders andother useful tools for managing your business easy',
+      join_us: 'Join Now for Free',
+      calendar_icon: 'Calendar picture'
+    },
+    why: {
+      internal_link_name: 'WHY',
+      content: {
+        why_atmza: 'Why Atzmaim',
+        why_description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+        business: 'Business Management',
+        appointments: 'Appointments Calendar',
+        notifications: 'SMS Notifications',
+        subscriptions: 'Client’s Subscriptions',
+        management: 'Client’s Management',
+        reminders: 'Tasks and Reminders'
+      }
+    },
+    showcases: {
+      internal_link_name: 'SHOWCASES',
+      main_title: 'App that makes business easy',
+      learn_more: 'Learn More',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua',
+      icon_alt_text: 'Iphone icon'
+    },
+    business_types: {
+      internal_link_name: 'BUSINESS TYPES',
+      main_title: 'Business Types',
+      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+      content: {   // * 6 items
+        hair_salons: {
+          title: 'Hair Salons Title',
+          text: 'Hair Salons Text',
+          alt: 'Hair Salons Alt',
+        },
+        massage_centers: {
+          title: 'Massage Centers Title',
+          text: 'Massage Centers Text',
+          alt: 'Massage Centers Alt',
+        },
+        sports_fitness: {
+          title: 'Sports Fitness Title',
+          text: 'Sports Fitness Text',
+          alt: 'Sports Fitness Alt',
+        },
+      }
+    },
+    feedback: {
+      internal_link_name: 'FEEDBACK',
+      alt_pic: 'User avatar',
+      main_title: 'Feedback from our customers',
+      subtitle: `Here you can leave your feedback about our
+    product. Reviews are publishing instantly`,
+      leave_btn_label: 'Leave Feedback'
+    },
+    footer: {
+      copy_right: '{year} Atzmaim | All right reserved',
+      twitter: 'Twitter',
+      facebook: 'Facebook',
+      instagram: 'Instagram'
+    }
+  },
+ he: {
+    hero: {
+      internal_link_name: 'HERO',
+      main_logo: 'ATZMAIM',
+      main_logo_label: 'ATZMAIM Logo',
+      main_title: 'Manage Business Easily',
+      log_in: 'Log in',
+      sign_up: 'Sign Up',
+      description: 'We’ve created for you an application with calendar,clients appointments, automated text reminders andother useful tools for managing your business easy',
+      join_us: 'Join Now for Free',
+      calendar_icon: 'Calendar picture'
+    },
+    why: {
+      internal_link_name: 'WHY',
+      content: {
+        why_atmza: 'Why Atzmaim',
+        why_description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+        business: 'Business Management',
+        appointments: 'Appointments Calendar',
+        notifications: 'SMS Notifications',
+        subscriptions: 'Client’s Subscriptions',
+        management: 'Client’s Management',
+        reminders: 'Tasks and Reminders'
+      }
+    },
+    showcases: {
+      internal_link_name: 'SHOWCASES',
+      main_title: 'App that makes business easy',
+      learn_more: 'Learn More',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod tempor incididunt ut labore etdolore magna aliqua',
+      icon_alt_text: 'Iphone icon'
+    },
+    business_types: {
+      internal_link_name: 'BUSINESS TYPES',
+      main_title: 'Business Types',
+      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+      content: {   // * 6 items
+        hair_salons: {
+          title: 'Hair Salons Title',
+          text: 'Hair Salons Text',
+          alt: 'Hair Salons Alt',
+        },
+        massage_centers: {
+          title: 'Massage Centers Title',
+          text: 'Massage Centers Text',
+          alt: 'Massage Centers Alt',
+        },
+        sports_fitness: {
+          title: 'Sports Fitness Title',
+          text: 'Sports Fitness Text',
+          alt: 'Sports Fitness Alt',
+        },
+      }
+    },
+    feedback: {
+      internal_link_name: 'FEEDBACK',
+      alt_pic: 'User avatar',
+      main_title: 'Feedback from our customers',
+      subtitle: `Here you can leave your feedback about our
+    product. Reviews are publishing instantly`,
+      leave_btn_label: 'Leave Feedback'
+    },
+    footer: {
+      copy_right: '{year} Atzmaim | All right reserved',
+      twitter: 'Twitter',
+      facebook: 'Facebook',
+      instagram: 'Instagram'
+    }
+  },
+}
