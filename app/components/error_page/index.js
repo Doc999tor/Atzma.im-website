c6d30926b1c41dcbecdb49829e8a96798604e0ce
tkachuks <serhiytkachuk88@gmail.com>
2020-01-23 22:06:47 +0200

    added footer to error page

--- app/components/error_page/index.js
+++ app/components/error_page/index.js
@@ -1,13 +1,15 @@
 import Header from '../contact-us/components/header/index.jsx'
 import Error from '../../../components-lib/error'
+import Footer from '../footer/footer.jsx'
 import './error.styl'
 export default ({ referer }) => {
   const possibleKeys = ['hero', 'features', 'showcases', 'business_types', 'feedback']
   const componentsForRendering = possibleKeys.filter(pk => config.modules[pk])
   return (
-    <div>
+    <div className='error_route'>
       <Header links={componentsForRendering} />
       <Error referer={referer} />
+      <Footer />
     </div>
   )
 }
