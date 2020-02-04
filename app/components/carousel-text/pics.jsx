536735075aaea864a004617b66d8ba70df01ee65
tkachuks <serhiytkachuk88@gmail.com>
2020-02-04 23:12:42 +0200

    upd showcases

--- app/components/carousel-text/pics.jsx
+++ app/components/carousel-text/pics.jsx
@@ -4,7 +4,7 @@ export default ({ item }) => {
   return (
     <div className={'slide ' + (config.isRTL ? 'rtl-slide' : 'ltr-slide')}>
       <picture>
-        <source src={`${config.urls.hero_carousel}${item.icon}.webp`} type='image/webp' />
+        <source srcSet={`${config.urls.hero_carousel}${item.icon}.webp`} type='image/webp' />
         <img className='carousel-img' src={`${config.urls.hero_carousel}${item.icon}.png`} alt={item.icon} />
       </picture>
     </div>
--- app/components/showcases/showcases.jsx
+++ app/components/showcases/showcases.jsx
@@ -20,12 +20,17 @@ export default class Topnav extends React.Component {
               {config.translations.hero.try_free}</a>
           </div>
           <div className='img-container'>
-            {config.modules.showcases.data.map((picName, i) => (
-              <picture key={i}>
-                <source src={`${config.urls.media_showcases}${picName}.webp`} type='image/webp' alt={picName}/>
-                <img className={'icon-' + (i + 1) + (config.isRTL ? ' rtl-icon-' + (i + 1) : ' ltr-icon-' + (i + 1))} src={`${config.urls.media_showcases}${picName}.png`} alt={picName} loading='lazy' />
-                <img className={'frame-' + (i + 1) + (config.isRTL ? ' rtl-frame-' + (i + 1) : ' ltr-frame-' + (i + 1))} src={config.urls.media_showcases + 'iphone-mockup.png'} />
-              </picture>
+            {config.modules.showcases.phones_pics.map((picName, i) => (
+              <React.Fragment>
+                <picture key={picName}>
+                  <source srcSet={`${config.urls.media_showcases}${picName}.webp`} className={'icon-' + (i + 1) + (config.isRTL ? ' rtl-icon-' + (i + 1) : ' ltr-icon-' + (i + 1))} alt={config.translations.showcases.phones_pics_alt[i]} type='image/webp' loading='lazy' />
+                  <img className={'icon-' + (i + 1) + (config.isRTL ? ' rtl-icon-' + (i + 1) : ' ltr-icon-' + (i + 1))} src={`${config.urls.media_showcases}${picName}.png`} alt={config.translations.showcases.phones_pics_alt[i]} loading='lazy' />
+                </picture>
+                <picture key={i + 'frame'}>
+                  <source srcSet={config.urls.media_showcases + 'iphone-mockup.webp'} className={'frame-' + (i + 1) + (config.isRTL ? ' rtl-frame-' + (i + 1) : ' ltr-frame-' + (i + 1))} type='image/webp' loading='lazy' />
+                  <img className={'frame-' + (i + 1) + (config.isRTL ? ' rtl-frame-' + (i + 1) : ' ltr-frame-' + (i + 1))} src={config.urls.media_showcases + 'iphone-mockup.png'} loading='lazy' />
+                </picture>
+              </React.Fragment>
             ))}
           </div>
         </div>
