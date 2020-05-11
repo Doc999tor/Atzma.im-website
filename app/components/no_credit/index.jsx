514367abcd47abbcb840709b737b0e39113fe479
tkachuks <serhiytkachuk88@gmail.com>
2020-05-11 17:18:59 +0300

    upd styles for no_credit strip

--- app/components/no_credit/index.jsx
+++ app/components/no_credit/index.jsx
@@ -2,14 +2,19 @@ import BtnTryFree from '../btn-try-free/index.jsx'
 import './no_credit.styl'
 
 export default () => {
-  const wave1 = config.urls.media + 'wave_1.svg'
-  const wave2 = config.urls.media + 'wave_2.svg'
-  const background = { backgroundImage: `url(${wave1}), url(${wave2}), linear-gradient(123deg, #591ec0, #6623db 28%, #7d3ee8 54%, #be95ff 113%)` }
   return (
-    <div id='no_credit' style={background}>
+    <div id='no_credit'>
+      <img className='wave_1' src={`${config.urls.media}wave_1.svg`} alt='wave' loading='lazy' />
+      <img className='wave_2' src={`${config.urls.media}wave_2.svg`} alt='wave' loading='lazy' />
+      <div className='glow_1' />
+      <div className='glow_2' />
+      <div className='glow_3' />
+      <div className='glow_4' />
       <div className='no_credit-content'>
-        <p>{config.translations.no_credits.first_part_title}</p>
-        <p>{config.translations.no_credits.second_part_title}</p>
+        <h2>
+          <span>{config.translations.no_credits.first_part_title}</span>
+          <span>{config.translations.no_credits.second_part_title}</span>
+        </h2>
         <BtnTryFree label={config.translations.no_credits.button_label} />
       </div>
       <div className={'img-compound' + (config.isRTL ? ' compound-dir-rtl' : ' compound-dir-ltr')}>
