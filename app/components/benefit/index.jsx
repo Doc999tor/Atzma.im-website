33a5182027f74e214e85a1c496747136be1b5eb8
tkachuks <serhiytkachuk88@gmail.com>
2020-05-29 09:38:38 +0300

    added benefit strip

---
+++ app/components/benefit/index.jsx
@@ -0,0 +1,63 @@
+import './benefit.styl'
+
+export default class Benefit extends React.Component {
+  state = {
+    current: 0,
+    speed: 4800,
+  };
+
+  componentDidMount () {
+    this.startRotation()
+  }
+
+  componentWillUnmount () {
+    this.stopRotation()
+  }
+
+  startRotation = () => {
+    this.interval = setInterval(this.next, this.state.speed)
+  }
+
+  stopRotation = () => {
+    clearInterval(this.interval)
+  }
+
+  next = () => {
+    const current = this.state.current
+    let nextSlide = current + 1
+
+    if (nextSlide > config.translations.main_benefit.content.data.length - 1) {
+      nextSlide = 0
+    }
+
+    this.setState({
+      current: nextSlide
+    })
+  }
+
+  isActive = slide => this.state.current === slide
+
+  render () {
+    return (
+      <div id='benefit'>
+        {config.modules.main_benefit.data.map((item, index) => {
+          return (
+            <div key={index} className={'slide' + (this.isActive(index) ? ' active' : '')}>
+              <div className='pic_container'>
+                <img src={config.urls.media_benefit + item.pic} alt={config.translations.main_benefit.content.data[index]?.alt} />
+              </div>
+              <div className='texts'>
+                <div className='title-container'>
+                  <h2>{config.translations.main_benefit.content.data[index]?.title}</h2>
+                </div>
+                <div className='text-container'>
+                  <p>{config.translations.main_benefit.content.data[index]?.text}</p>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+}
--- app/components/home/home.jsx
+++ app/components/home/home.jsx
@@ -3,6 +3,7 @@ import Features from '../features/features.jsx'
 import Showcases from '../showcases/showcases.jsx'
 import BusinessTypes from '../business-types/business-types.jsx'
 import Reviews from '../reviews/index.jsx'
+import Benefit from '../benefit/index.jsx'
 import NoCredit from '../no_credit/index.jsx'
 import Footer from '../footer/footer.jsx'
 import ContactButton from '../btn-contact-us/index.jsx'
@@ -10,11 +11,12 @@ import './home.styl'
 
 class Home extends React.Component {
   render () {
-    const possibleKeys = ['hero', 'features', 'showcases', 'business_types', 'feedback', 'no_credits']
+    const possibleKeys = ['hero', 'features', 'main_benefit', 'showcases', 'business_types', 'feedback', 'no_credits']
     const componentsForRendering = possibleKeys.filter(pk => config.modules[pk])
     const objSplitLoadingComponents = {
       hero: <Hero links={componentsForRendering} />,
       features: <Features />,
+      main_benefit: <Benefit />,
       showcases: <Showcases />,
       business_types: <BusinessTypes />,
       feedback: <Reviews />,
