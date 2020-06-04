5d72f760c7ea08b3924e3d0c0f1d9106d1630886
tkachuks <serhiytkachuk88@gmail.com>
2020-06-04 13:36:36 +0300

    api connected to lead strip

--- app/components/lead/components/lead_content/index.jsx
+++ app/components/lead/components/lead_content/index.jsx
@@ -1,22 +1,76 @@
+import { getCurrentFormatTime } from 'project-services/helpers'
+import validatePhone from 'project-components/validate-phone'
+import { postService } from 'project-services/send_mail'
 import './lead_content.styl'
+const { useState } = React
 
-export default () => {
+export default ({ onSetSendingStatus, onSetSendedtatus }) => {
+  const [nameValue, setName] = useState('')
+  const handleSetName = e => {
+    const value = e.target.value
+    setValidName(true)
+    setName(value)
+  }
+  const [contactValue, setContact] = useState('')
+  const handleSetContact = e => {
+    const value = e.target.value
+    setValidValue(true)
+    setContact(value)
+  }
+  const [contactValid, setValidValue] = useState(true)
+  const [nameValid, setValidName] = useState(true)
+  const handleValidateContact = () => {
+    const regMail = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{1,6})/
+    if (validatePhone(contactValue.trim()) || regMail.test(contactValue.trim())) {
+      setValidValue(true)
+      return true
+    } else {
+      setValidValue(false)
+      return false
+    }
+  }
+  const handleValidateName = () => nameValue ? setValidName(true) : setValidName(false)
+  const handleSubmit = e => {
+    e.preventDefault()
+    handleValidateContact()
+    handleValidateName()
+    if (contactValid && nameValid && nameValue && handleValidateContact()) {
+      onSetSendingStatus(true)
+      setTimeout(() => {
+        const body = `name=${nameValue.trim()}&contact_detail=${contactValue.trim()}&added=${getCurrentFormatTime()}`
+        postService(config.urls.api_leads, body).then(r => {
+          if (r.status === 201) {
+            onSetSendedtatus(false)
+            setTimeout(() => {
+              onSetSendingStatus(false)
+            }, 2000)
+          }
+        })
+      }, 1000)
+    }
+  }
   return (
     <section className='lead_content'>
       <h2 className='lead_title'>{config.translations.lead.main_title}</h2>
       <p className='lead_subtitle'>{config.translations.lead.subtitle}</p>
-      <form onSubmit={e => e.preventDefault()}>
+      <form onSubmit={handleSubmit}>
         <div className='lead_inputs'>
           <input
+            className={nameValid ? 'normal_input' : 'warning_name'}
             type='text'
+            value={nameValue}
+            onChange={handleSetName}
             placeholder={config.translations.lead.placeholder_name}
           />
           <input
+            className={contactValid ? 'normal_input' : 'warning_contact'}
             type='text'
+            value={contactValue}
+            onChange={handleSetContact}
             placeholder={config.translations.lead.placeholder_contact}
           />
         </div>
-        <button className='submit_btn' type='submit'>
+        <button className={'submit_btn' + (!contactValid || !nameValid ? ' inactive' : '')} type='submit'>
           <span className='icon-send'>
             <svg>
               <use xlinkHref={config.urls.media + 'ic_send_btn.svg#ic_send'} />
--- app/components/lead/index.jsx
+++ app/components/lead/index.jsx
@@ -1,11 +1,19 @@
 import LeadContent from './components/lead_content/index.jsx'
+import SendModal from '../contact-us/components/send_modal/index.jsx'
 import './lead.styl'
+const { useState } = React
 
 export default () => {
+  const [sendingStatus, setSendingStatus] = useState(false)
+  const [sendedStatus, setSendedtatus] = useState(true)
+  const handleSetSendingStatus = bool => setSendingStatus(bool)
+  const handleSetSendedtatus = bool => setSendedtatus(bool)
   return (
     <div id='lead'>
       <img className='wave_left' src={config.urls.media + 'wave_left.svg'} />
-      <LeadContent />
+      {sendingStatus
+        ? <SendModal sending={sendedStatus} />
+        : <LeadContent onSetSendingStatus={handleSetSendingStatus} onSetSendedtatus={handleSetSendedtatus} />}
       <img className='wave_right' src={config.urls.media + 'wave_right.svg'} />
     </div>
   )
