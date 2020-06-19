211c668f2d249dda103b7c697fb8e51793620741
tkachuks <serhiytkachuk88@gmail.com>
2020-06-19 16:17:09 +0300

    fix render discount value

--- app/components/pricing/components/price.jsx
+++ app/components/pricing/components/price.jsx
@@ -1,10 +1,9 @@
 import './price.styl'
 
-const Price = ({ name, icon, text, value, discount, toggleSwitch }) => {
-  const basic = name === 'basic'
-  const marker = {'listStyleImage': 'url(' + config.urls.media + (basic ? 'ic_check_mark_active.svg' : 'ic_check_mark.svg' ) + ')'};
+const Price = ({ preferred, name, icon, text, value, discount, toggleSwitch }) => {
+  const marker = {'listStyleImage': 'url(' + config.urls.media + (preferred ? 'ic_check_mark_active.svg' : 'ic_check_mark.svg' ) + ')'};
   return (
-    <div className={'price' + (basic ? ' basic_price' : '')}>
+    <div className={'price' + (preferred ? ' basic_price' : '')}>
       {discount && toggleSwitch && <div className='discount'><span>{discount}</span><span className='label'>{config.translations.pricing.discount_label}</span></div>}
       <div className='icon_wrap'>
         <img src={config.urls.media + icon} alt={icon} />
@@ -17,12 +16,11 @@ const Price = ({ name, icon, text, value, discount, toggleSwitch }) => {
       <div className='price_info'>
         <div className='price_count'>
           <p>
-            <span className='count'>{
-              basic
-                ? config.translations.pricing.data[name].opened_preview.group_preview_price
-                : config.translations.pricing.data[name].opened_preview.group_preview_price.replace('{currency}', config.modules.pricing.currency)
-                  .replace('{price_value}', value)
-            }
+            <span className='count'>
+              {
+                config.translations.pricing.data[name].opened_preview.group_preview_price.replace('{currency}', config.modules.pricing.currency)
+                .replace('{price_value}', value)
+              }
             </span>
             <span className='bill_count'>{text}</span>
           </p>
