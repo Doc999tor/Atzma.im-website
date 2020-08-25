2ef698b526d4e932803f921e1d86ca82681a5c58
tkachuks <serhiytkachuk88@gmail.com>
2020-08-25 11:56:53 +0300

    inline svg for red buttons

--- app/components/leads/components/leads_content/index.jsx
+++ app/components/leads/components/leads_content/index.jsx
@@ -85,7 +85,11 @@ export default ({ phone, nameLable, contactLable, onSetSendingStatus, onSetSende
         </div>
         <button className={'submit_btn' + ((!contactValid || !nameValid) && openedPopup ? ' inactive' : '')} type='submit'>
           <span className='icon-send'>
-            <img src={config.urls.media + 'ic_send.svg'} />
+            <svg xmlns='http://www.w3.org/2000/svg' width='19.178' height='19.007' viewBox='0 0 19.178 19.007'>
+              <g id='prefix__ic_send' transform='translate(.579 .507)'>
+                <path id='prefix__ic_send-2' d='M17.811 14.353a1.911 1.911 0 0 0-.875-.9L2.708 6.391a1.865 1.865 0 0 0-2.519.9 1.981 1.981 0 0 0-.054 1.56L2.609 15.2.135 21.541a1.956 1.956 0 0 0 1.059 2.519A1.831 1.831 0 0 0 2.708 24l14.228-7.066a1.958 1.958 0 0 0 .875-2.581zM2.16 22.84a.622.622 0 0 1-.84-.3.66.66 0 0 1-.02-.52l2.409-6.179h12.55zm1.551-8.289L1.3 8.372a.64.64 0 0 1 .155-.709.6.6 0 0 1 .7-.109l14.1 7z' data-name='ic_send' transform='translate(0 -6.197)'/>
+              </g>
+            </svg>
           </span>
           <span className='btn_label'>{btnLabel}</span>
         </button>
