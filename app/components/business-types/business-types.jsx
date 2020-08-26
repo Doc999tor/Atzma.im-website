ea5143f1f484ad6a04cc4ea2f2387e47c8945c49
tkachuks <serhiytkachuk88@gmail.com>
2020-08-26 17:18:52 +0300

    added business_types_mobile strip

--- app/components/business-types/business-types.jsx
+++ app/components/business-types/business-types.jsx
@@ -1,78 +1,16 @@
-import React, { useState } from 'react'
-import { Swiper, Autoplay, Navigation, Pagination } from 'swiper/js/swiper.esm'
-import ReactIdSwiperCustom from 'react-id-swiper/lib/ReactIdSwiper.custom'
+import React from 'react'
 import './business-types.styl'
 import 'swiper/css/swiper.css'
 
 const BusinessTypes = () => {
-  const [slides, setSlides] = useState([...config.modules.business_types.data])
-  const params = {
-    Swiper,
-    slidesPerView: 4,
-    slidesPerGroup: 4,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: false
-    },
-    autoplay: {
-      delay: config.modules.business_types.carousel_time || 5000,
-      disableOnInteraction: false
-    },
-    loop: true,
-    loopFillGroupWithBlank: true,
-    breakpoints: {
-      1366: {
-        slidesPerView: 4,
-        slidesPerGroup: 4
-      },
-      1024: {
-        slidesPerView: 3,
-        slidesPerGroup: 3
-      },
-      640: {
-        slidesPerView: 2,
-        slidesPerGroup: 2
-      },
-      320: {
-        slidesPerView: 1,
-        slidesPerGroup: 1
-      }
-    },
-    noSwiping: false,
-    modules: [Navigation, Autoplay, Pagination],
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    },
-    renderPrevButton: () => <div className='swiper-button-prev swiper-no-swiping' />,
-    renderNextButton: () => <div className='swiper-button-next swiper-no-swiping' />
-  }
   return (
     <div id='business_types'>
       <header className='header'>
-        <h2>{config.translations.business_types.main_title}</h2>
-        <p>{config.translations.business_types.subtitle}</p>
+        <h2>{config.translations.business_types_mobile.main_title}</h2>
+        <p>{config.translations.business_types_mobile.subtitle}</p>
       </header>
       <div className='content-box'>
-        {slides.length > 0 && <ReactIdSwiperCustom {...params}>
-          {slides.map((item, index) => {
-            return (
-              <div className='outer-wrap' key={index}>
-                <figure className='slide'>
-                  <picture>
-                    <source srcSet={config.urls.media_business_types + item.icon + '.webp'} type='image/webp' alt={config.translations.business_types.content[item.name].title} />
-                    <img src={config.urls.media_business_types + item.icon + '.jpg'} alt={config.translations.business_types.content[item.name].title} />
-                  </picture>
-                  <figcaption>
-                    <h3>{config.translations.business_types.content[item.name].title}</h3>
-                    <p>{config.translations.business_types.content[item.name].text}</p>
-                  </figcaption>
-                </figure>
-              </div>
-            )
-          })}
-        </ReactIdSwiperCustom>}
+        <img src={config.urls.media + 'business_types.png'} alt='' role='presentation' />
       </div>
     </div>
   )
--- app/components/home/home.jsx
+++ app/components/home/home.jsx
@@ -4,15 +4,14 @@ import Features from '../features/features.jsx'
 import Showcases from '../showcases/showcases.jsx'
 import Leads from '../leads/index.jsx'
 import BusinessTypes from '../business-types/business-types.jsx'
-//import Reviews from '../reviews/index.jsx'
 import Benefit from '../benefit/index.jsx'
 import NoCredit from '../no_credit/index.jsx'
 import Footer from '../footer/footer.jsx'
 import ContactButton from '../btn-contact-us/index.jsx'
 import './home.styl'
 
-const Home = ({ history }) => {
-  const possibleKeys = ['hero', 'features', 'main_benefit', 'leads', 'showcases', 'business_types', 'feedback', 'no_credits']
+const Home = () => {
+  const possibleKeys = ['hero', 'features', 'main_benefit', 'leads', 'showcases', 'business_types_mobile', 'no_credits']
   const componentsForRendering = possibleKeys.filter(pk => config.modules[pk])
   const objSplitLoadingComponents = {
     hero: <Hero links={componentsForRendering} />,
@@ -20,8 +19,7 @@ const Home = ({ history }) => {
     main_benefit: <Benefit />,
     leads: <Leads />,
     showcases: <Showcases />,
-    business_types: <BusinessTypes />,
-    //feedback: <Reviews />,
+    business_types_mobile: <BusinessTypes />,
     no_credits: <NoCredit />
   }
   return (
@@ -29,7 +27,7 @@ const Home = ({ history }) => {
       {
         componentsForRendering.map(i => objSplitLoadingComponents[i])
       }
-      <ContactButton history={history} />
+      <ContactButton />
       <Footer />
     </div>
   )
