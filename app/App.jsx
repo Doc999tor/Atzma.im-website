c244fcbe46bc33d3bc46c6f6a81bee2c1ddcc062
tkachuks <serhiytkachuk88@gmail.com>
2020-08-26 09:28:25 +0300

    delete fn onload

--- app/App.jsx
+++ app/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/home/home.jsx'
 import './main.styl'
 class App extends Component {
   render () {
+    document.getElementsByTagName('body')[0].style.direction = config.isRTL ? 'rtl' : 'ltr'
     return <Home />
   }
 }
--- app/main.js
+++ app/main.js
@@ -1,9 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
-document.body.onload = function () {
-  document.getElementsByTagName('body')[0].style.direction = config.isRTL ? 'rtl' : 'ltr'
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root'))
-}
+ReactDOM.render(<App />, document.getElementById('root'))
