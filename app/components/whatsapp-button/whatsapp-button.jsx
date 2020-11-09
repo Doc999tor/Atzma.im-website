daf2451642dcc97f14150c0686e74dee7513ee4b
tkachuks <serhiytkachuk88@gmail.com>
2020-11-09 10:05:41 +0200

    fix rtl styles for WA button

--- app/components/whatsapp-button/whatsapp-button.jsx
+++ app/components/whatsapp-button/whatsapp-button.jsx
@@ -4,7 +4,7 @@ import './whatsapp-button.styl'
 
 const WSbutton = ({ showButton }) => {
   return (
-    <a href={config.urls.wa_api.replace('{phone}', config.modules.contact_us.phone_number.slice(1))} id='whatsapp-button' className={config.isRTL ? 'position_rtl' : 'position_ltr' + (showButton ? ' show_ws_button' : '')}>
+    <a href={config.urls.wa_api.replace('{phone}', config.modules.contact_us.phone_number.slice(1))} id='whatsapp-button' className={(config.isRTL ? 'position_rtl' : 'position_ltr') + (showButton ? ' show_ws_button' : '')}>
       <img src={`${config.urls.media}ic_whatsapp_btn.svg`} />
     </a>
   )
