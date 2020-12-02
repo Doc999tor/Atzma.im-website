6f71aac2a4bf878fd77ad192864ff741c9be7364
tkachuks <serhiytkachuk88@gmail.com>
2020-12-03 01:22:06 +0200

    ups showcases

--- app/components/showcases/showcases.jsx
+++ app/components/showcases/showcases.jsx
@@ -7,16 +7,16 @@ export default class Topnav extends Component {
       <div id='showcases'>
         <div className='main-box'>
           <div className='sub-box'>
-            <h2>{config.translations.showcases_desktop.main_title}</h2>
-            <p>{config.translations.showcases_desktop.description}</p>
-            <BtnTryFree label={config.translations.showcases_desktop.button_label} />
+            <h2>{config.translations.showcases.main_title}</h2>
+            <p>{config.translations.showcases.description}</p>
+            <BtnTryFree label={config.translations.showcases.button_label} />
           </div>
           <div className='img-container'>
-            {config.modules.showcases_desktop.phones_pics.map((picName, i) => (
+            {config.modules.showcases.phones_pics.map((picName, i) => (
               <>
                 <picture key={picName}>
-                  <source srcSet={`${config.urls.media_showcases}${picName}.webp`} className={'icon-' + (i + 1) + (config.isRTL ? ' rtl-icon-' + (i + 1) : ' ltr-icon-' + (i + 1))} alt={config.translations.showcases_desktop.phones_pics_alt[i]} type='image/webp' loading='lazy' />
-                  <img className={'icon-' + (i + 1) + (config.isRTL ? ' rtl-icon-' + (i + 1) : ' ltr-icon-' + (i + 1))} src={`${config.urls.media_showcases}${picName}.png`} alt={config.translations.showcases_desktop.phones_pics_alt[i]} loading='lazy' />
+                  <source srcSet={`${config.urls.media_showcases}${picName}.webp`} className={'icon-' + (i + 1) + (config.isRTL ? ' rtl-icon-' + (i + 1) : ' ltr-icon-' + (i + 1))} alt={config.translations.showcases.phones_pics_alt[i]} type='image/webp' loading='lazy' />
+                  <img className={'icon-' + (i + 1) + (config.isRTL ? ' rtl-icon-' + (i + 1) : ' ltr-icon-' + (i + 1))} src={`${config.urls.media_showcases}${picName}.png`} alt={config.translations.showcases.phones_pics_alt[i]} loading='lazy' />
                 </picture>
               </>
             ))}
